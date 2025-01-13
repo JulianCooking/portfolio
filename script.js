20ff7a1c94d4fde834bda0b6ed5ea86034b6b2f1
JulianCooking <2601julian@gmail.com>
2025-01-13 11:07:49 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let naam = document.querySelector("#naam")
+naam.style.fontFamily = "monospace";
+
+naam.addEventListener("click", function() {
+    window.location.href = "http://julianvs.nl/aboutme"; // Vervang door de URL waar je naartoe wilt navigeren
+});
