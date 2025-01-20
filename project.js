b6933ef055d091e4ecabd614bd7c0b6d7eb2630a
JulianCooking <2601julian@gmail.com>
2025-01-20 15:07:00 +0100

    Add files via upload

---
+++ project.js
@@ -0,0 +1,26 @@
+
+let pp = document.querySelector("#pp")
+
+pp.addEventListener("click", function() { 
+    window.location.href = "pp.html";
+});
+
+let robot = document.querySelector("#robot")
+
+robot.addEventListener("click", function() { 
+    window.location.href = "robot.html";
+});
+
+
+let sport = document.querySelector("#sport")
+
+sport.addEventListener("click", function() { 
+    window.location.href = "sport.html";
+});
+
+
+let film = document.querySelector("#film")
+
+film.addEventListener("click", function() { 
+    window.location.href = "film.html";
+});
