19d6f8dd64e2167335ec8f9adbc73c56a7d29149
JulianCooking <2601julian@gmail.com>
2025-01-20 17:07:18 +0100

    Update project.js

--- project.js
+++ project.js
@@ -24,3 +24,18 @@ let film = document.querySelector("#film")
 film.addEventListener("click", function() { 
     window.location.href = "film.html";
 });
+
+
+/* ------------------ kop knopjes -------------- */
+
+let homeknop = document.querySelector(".homeknop")
+
+homeknop.addEventListener("click", function() { 
+    window.location.href = "index.html";
+});
+
+let projectknop = document.querySelector(".projectknop")
+
+projectknop.addEventListener("click", function() { 
+    window.location.href = "projects.html";
+});
